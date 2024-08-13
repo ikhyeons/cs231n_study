8714a1053f77906a7c25b9a1a7834a811071cc68
ikhyeons <skantrkwl789@naver.com>
2024-08-13 16:57:43 +0900

    Fix : Class Name

--- lect2/LinearClassifier.py
+++ lect2/LinearClassifier.py
@@ -1,8 +1,8 @@
-import numpy as np;
+import numpy as np
 from sklearn.datasets import make_classification
 
 
-class LinearClassifier:
+class KNN:
     def __init__(self, dataset):
         self.success = 0
 
@@ -13,11 +13,8 @@ class LinearClassifier:
         #0 ~ 9
         self.Y = np.array(self.dataset[1])
 
-        self.trainX = np.array(self.X[:35000])
-        self.trainY = np.array(self.Y[:35000])
-
-        self.validateX = np.array(self.X[35001:45000])
-        self.validateY = np.array(self.Y[35001:45000])
+        self.trainX = np.array(self.X[:45000])
+        self.trainY = np.array(self.Y[:45000])
 
         self.testX = np.array(self.X[45001:])
         self.testY = np.array(self.Y[45001:])
@@ -27,7 +24,6 @@ class LinearClassifier:
 
     #input is len 15 array
     def run(self, k, input):
-
         #각 데이터로부터 거리 구하기
         distance = np.sqrt(np.sum((self.trainX - self.testX[input])**2, axis=1))
         #다른 정도가 제일 작은 데이터 k개 구하기
@@ -39,9 +35,9 @@ class LinearClassifier:
             self.success += 1
 
 
-linear_classifier = LinearClassifier(make_classification(n_samples=50000, n_features=15, n_informative=5, n_classes=10, random_state=40))
+knn = KNN(make_classification(n_samples=50000, n_features=15, n_informative=5, n_classes=10, random_state=40))
 
-for i in range(len(linear_classifier.testX)):
-    linear_classifier.run(50, i);
+for i in range(len(knn.testX)):
+    knn.run(50, i);
 
-print("%s %%" % (linear_classifier.success / 5000 * 100))
+print("%s %%" % (knn.success / 5000 * 100))
